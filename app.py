0d302a7d3ca2352d688f73a988f69b5ec18f67fe
Michaelliem99 <43264172+Michaelliem99@users.noreply.github.com>
2023-03-20 20:55:44 +0700

    Update data source

--- app.py
+++ app.py
@@ -5,25 +5,30 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.responses import Response, JSONResponse
 from datetime import date, datetime
 from fastapi import Depends, HTTPException
+import os
 
 app = FastAPI()
 
+engine = create_engine(
+    "postgresql://{}:{}@{}/{}".format(
+        os.getenv('POSTGRE_USER'), os.getenv('POSTGRE_PW'), os.getenv('POSTGRE_HOST'), os.getenv('POSTGRE_DB')
+    )
+)
+conn = engine.connect()
 
 @app.get("/company_profiles")
 async def get_company_profiles(StockCode: Optional[str] = None, Sektor: Optional[str] = None, SubSektor: Optional[str] = None):
     # Your code here to retrieve the company profiles based on the parameters
     # and format the data as a JSON response
-
-    company_profiles = pd.read_excel('stocks.xlsx', sheet_name='Company Profiles')
     
     if StockCode:
-        output_df = company_profiles[company_profiles['StockCode'] == StockCode]
+        output_df = pd.read_sql('SELECT * FROM IDXCompanyProfiles WHERE StockCode = {}'.format(StockCode), con=conn)
     elif Sektor:
-        output_df = company_profiles[company_profiles['Sektor'] == Sektor]
+        output_df = pd.read_sql('SELECT * FROM IDXCompanyProfiles WHERE Sektor = {}'.format(Sektor), con=conn)
     elif SubSektor:
-        output_df = company_profiles[company_profiles['SubSektor'] == SubSektor]
+        output_df = pd.read_sql('SELECT * FROM IDXCompanyProfiles WHERE SubSektor = {}'.format(SubSektor), con=conn)
     else:
-        output_df = company_profiles
+        output_df = pd.read_sql('SELECT * FROM IDXCompanyProfiles', con=conn)
     
     return Response(output_df.to_json(orient="records"), media_type="application/json")
 
@@ -33,14 +38,7 @@ async def get_trading_info(StockCode: str, StartDate: date, EndDate: date):
     # Your code here to retrieve the company profiles based on the parameters
     # and format the data as a JSON response
 
-    trading_info = pd.read_excel('stocks.xlsx', sheet_name='Trading Info')
-
-    # Filter by StockCode
-    output_df = trading_info[trading_info['StockCode'] == StockCode]
-
-    # Filter by StartDate and EndDate if both are provided
-    if StartDate and EndDate:
-        mask = (output_df['Date'] >= pd.to_datetime(StartDate)) & (output_df['Date'] <= pd.to_datetime(EndDate))
-        output_df = output_df.loc[mask]
+    # Filter by StockCode and Date Range
+    output_df = pd.read_sql('SELECT * FROM IDXCompanyProfiles WHERE StockCode = {} and Date >= {} and Date <= {}'.format(StockCode, StartDate, EndDatte), con=conn)
 
     return Response(output_df.to_json(orient="records"), media_type="application/json")
